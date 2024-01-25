3782c856808d94ded4f90e8fd48ae2e2638d3bde
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2024-01-24 21:41:26 -0400

    Hasta la creación del nuevo proyecto

---
+++ Secundario/Controllers/PedidoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Secundario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        public PedidoController()
+        {
+            
+        }
+        [HttpPost]
+        public ActionResult Post()
+        {
+            Console.WriteLine("Llegó una petición al servicio Pedido");
+            return Ok("Respuesta exitosa desde el controlador Pedido");
+        }
+    }
+}
